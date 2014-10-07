becc0d1f3776883d4d27855f3b2e1ca268911aeb
Jacqui De Sa <jdesa@mit.edu>
2014-10-07 14:50:26 -0400

    Fixed typo in URL response

--- JacquiTest/cloud/main.js
+++ JacquiTest/cloud/main.js
@@ -23,55 +23,54 @@ Parse.Cloud.define("sendSMS", function (request, response) {
 });
 
 Parse.Cloud.define("receiveSMS", function (request, response) {
-        console.log("haz begun");
-        var helpStartSeq = "HelpMe";
-        var shelterStartSeq = "HelpOthers";
-        var Mouse = Parse.Object.extend("Mouse");
-        console.log("a");
-        var newEntry = new Mouse();
-        console.log("b");
-        var splitText = request.params.Body.split(".");
-        console.log(splitText);
-       if (splitText[0] == helpStartSeq) {
-            console.log("case 1");
+    console.log("haz begun");
+    var helpStartSeq = "HelpMe";
+    var shelterStartSeq = "HelpOthers";
+    var Mouse = Parse.Object.extend("Mouse");
+    console.log("a");
+    var newEntry = new Mouse();
+    console.log("b");
+    var splitText = request.params.Body.split(".");
+    console.log(splitText);
+    if (splitText[0] == helpStartSeq) {
+        console.log("case 1");
         try {
-        newEntry.set("Number", request.params.From);
-        newEntry.set("Name", splitText[1]);
+            newEntry.set("Number", request.params.From);
+            newEntry.set("Name", splitText[1]);
+            newEntry.set("Location", splitText[3]);
         } catch (err) {
-                console.log("Error");
-            }
+            console.log("Error");
+        }
         try {
-                newEntry.set("SmartPhone", splitText[2]);
-                            console.log("case 3");
+            newEntry.set("SmartPhone", splitText[2]);
+            console.log("case 3");
 
         } catch (err) {
             newEntry.set("SmartPhone", splitText[2]);
-                        console.log("case 4");
+            console.log("case 4");
 
         }
         try {
-                newEntry.set("HostAddress", splitText[3]);
-                newEntry.set("HostCapacity", splitText[4]);
-                            console.log("case 5");
+            newEntry.set("HostAddress", splitText[3]);
+            newEntry.set("HostCapacity", splitText[4]);
+            console.log("case 5");
+
+        } catch (err) { 
+            newEntry.set("HostAddress", "NotAHost");
+            newEntry.set("HostCapacity", "NotAHost");
+            console.log("case 6");
+        }
+        try {
+
+            newEntry.set("Official", splitText[5]);
+            console.log("case 7");
 
         } catch (err) {
-                newEntry.set("HostAddress", "NotAHost");
-                newEntry.set("HostCapacity", "NotAHost");
-                            console.log("case 6");
-
+            newEntry.set("Official", false);
+            console.log("case 8");
         }
-        try { 
-        
-                newEntry.set("Official", splitText[5]);
-                            console.log("case 7");
-    
-        } catch (err) {
-                newEntry.set("Official", false);
-                            console.log("case 8");
-        }
-            //Number, Name, Location, SmartPhone(Bool), Host Address, Host Capacity, Host (Bool)
-       }
-   else if (splitText[0] == shelterStartSeq) {
+        //Number, Name, Location, SmartPhone(Bool), Host Address, Host Capacity, Host (Bool)
+    } else if (splitText[0] == shelterStartSeq) {
         console.log("case 9");
 
     } else {
@@ -100,36 +99,50 @@ Parse.Cloud.define("receiveSMS", function (request, response) {
             response.error("!hamster");
         }
     });
-    /*newEntry.set("", console.log(request.params.Body));
-    (console.log(request.params.Body));
-  console.log(request.params.From);*/
 
+    // User's location
+    var userGeoPoint = splitText[2];
+    // Create a query for places
+    var query = new Parse.Query(userGeoPoint);
+    // Interested in locations near user.
+    query.near("Location", userGeoPoint);
+    // Limit what could be a lot of points.
+    query.limit(10);
+    // Final list of objects
+    query.find({
+        success: function (placesObjects) {}
+    });
+
+    for (var i=0; i<userGeoPoint.length; i++){
+        destinationAddress = userGeoPoint[i];
+        var URLString = "https://maps.googleapis.com/maps/api/directions/json?origin=" + 42.356 + "," + -71.096 + "&destination=" + "410 Memorial Drive Cambridge, MA" + "&key=" + "AIzaSyD7k5RekuUfdp4iqiXb0I-kjaxVBzLVzdo" + "&mode=" + "walking";
+        console.log(URLString);
+        console.log(Parse.Cloud.httpResponse(URLString).text);
+        var Mouse = Parse.Object.extend("");
+        console.log("a");
+        var newEntry = new Mouse();
+        
+        
+    }
+   // String URLString = "https://maps.googleapis.com/maps/api/directions/json?origin=" + currentLocation.getLatitude() + "," + currentLocation.getLongitude() + "&destination=" + destinationAddress + "&key=" + directionsKey;
+
+    //NORI'S CODE SO BEWARE!!!!!
+    
+    function getPage(request, response) { response.success("Hello world!") }
+
+    Parse.Cloud.define("getPage", function(request, response) {
+    
+        Parse.Cloud.httpRequest({
+            url: URLString,
+            success: function(httpResponse) {
+                console.log(httpResponse.text)
+            },
+            error: function(httpResponse) {
+                console.error('Request failed with response code ' + httpResponse.status)
+            }
+        });
+    
+
+    });
 });
-
-
-
-
-
-/*
-
-Parse.Cloud.define("receiveSMS", function(request, response) {
-  console.log("Received a new test!");
-  console.log(client);
-  // Send an SMS message
-    client.receiveSMS({
-        to:'15202618230', 
-        from: '15203086570', 
-        body: 'Hello world!'
-      }, function(err, responseData) { 
-        if (err) {
-          console.log(err);
-          response.error("Fail :("); 
-        } else { 
-          console.log(responseData.from); 
-          console.log(responseData.body);
-          response.success("jhjh"); 
-        }
-      }
-    );
-});
-*/
+//String URLString = "https://maps.googleapis.com/maps/api/directions/json?origin=" + currentLocation.getLatitude() + "," + currentLocation.getLongitude() + "&destination=" + destinationAddress + "&key=" + directionsKey;
